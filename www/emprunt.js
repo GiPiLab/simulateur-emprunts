e8d15a3ff8fc1914ee74335669039142b19aa07e
Thibault <thibault@gipilab.org>
2015-09-18 16:40:22 +0200

    Second commit

--- www/emprunt.js
+++ www/emprunt.js
@@ -10,6 +10,41 @@ function calculeEcheanceConstante(capital,tauxAnnuel,duree,periodicite)
 	return capital.times(ip).dividedBy(Decimal.ONE.minus(puissance));
 }
 
+function calculeEcheanceCapitalConstant(capital,tauxAnnuel,duree,periodicite)
+{
+	var ip=tauxAnnuel.dividedBy(periodicite);
+	return capital.times(ip).plus(capital.dividedBy(duree));
+}
+
+
+function calculeDureeEcheanceConstante(capital,tauxAnnuel,echeance,periodicite)
+{
+	var ip=tauxAnnuel.dividedBy(periodicite);
+	var denum=ip.times(capital).minus(echeance);
+	var tmp1=echeance.dividedBy(denum).neg().ln();
+	var tmp2=ip.plus(Decimal.ONE).ln();
+	return tmp1.dividedBy(tmp2).round(Decimal.ROUND_UP);
+}
+
+function calculeDureeCapitalConstant(capital,tauxAnnuel,echeance,periodicite)
+{
+	var ip=tauxAnnuel.dividedBy(periodicite);
+	return capital.dividedBy(echeance.minus(capital.times(ip))).round(Decimal.ROUND_UP);
+}
+
+function periodiciteToString(periodicite)
+{
+	var per=periodicite.toString();
+	switch(per)
+	{
+		case "12":return "mensuelle";
+		case "4":return "trimestrielle";
+		case "2":return "semestrielle";
+		case "1":return "annuelle";
+	}
+}
+
+
 
 function formatDureeEmprunt(duree,periodicite)
 {
